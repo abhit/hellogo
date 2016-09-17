4afcbd665d6ba854b362c3f051a653a858f135f8
abhit <abhishektyagi@grokBook.local>
2016-09-17 12:34:37 -0700

    my new changes

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello, world\n")
+	fmt.Printf("this is second hello, world\n")
+	fmt.Printf("this is third hello, world\n")
+	fmt.Printf("this is fourth hello, world\n")
+}
